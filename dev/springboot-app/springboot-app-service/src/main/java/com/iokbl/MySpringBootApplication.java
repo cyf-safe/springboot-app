7d9123f1c042a93e4217d179426adb27a34aed5a
cyf_safe <YuFei.Chen@bl.com>
2020-06-01 23:29:09 +0800

    添加Swagger2支持

--- dev/springboot-app/springboot-app-service/src/main/java/com/iokbl/MySpringBootApplication.java
+++ dev/springboot-app/springboot-app-service/src/main/java/com/iokbl/MySpringBootApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableSwagger2
 @EnableTransactionManagement
 public class MySpringBootApplication {
 
