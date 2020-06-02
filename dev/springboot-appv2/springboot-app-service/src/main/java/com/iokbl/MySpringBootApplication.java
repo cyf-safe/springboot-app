07c7ea40f62f147793a7e20f7b8c19c139eb6d71
cyf12 <YuFei.Chen@bl.com>
2020-06-02 19:36:10 +0800

    架构更新

--- dev/springboot-appv2/springboot-app-service/src/main/java/com/iokbl/MySpringBootApplication.java
+++ dev/springboot-appv2/springboot-app-service/src/main/java/com/iokbl/MySpringBootApplication.java
@@ -1,9 +1,7 @@
 package com.iokbl;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
