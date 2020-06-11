c0c2f3637fab6ef9bc9b21e5f0286f17ecb176b6
cyf12 <YuFei.Chen@bl.com>
2020-06-11 14:48:30 +0800

    动态刷新

--- dev/springboot-appv2/springboot-app-web/src/main/java/com/iokbl/config/ConfigConstants.java
+++ dev/springboot-appv2/springboot-app-web/src/main/java/com/iokbl/config/ConfigConstants.java
@@ -1,15 +1,18 @@
 package com.iokbl.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
+@RefreshScope
 public class ConfigConstants {
 
     @Value("${login.timeout.minutes}")
     private int loginTimeoutMinutes;
 
-    private String serviceName = "springboot-service";
+    @Value("${springboot-service-appid}")
+    private String serviceName;
 
     public int getLoginTimeoutMinutes() {
         return loginTimeoutMinutes;
